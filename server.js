c5e9bd72200edad28154ce7949382fce2c8a47ec
skahmerali <sheikhahmerali75@gmail.com>
2021-01-12 14:41:19 +0500

    class code

--- server.js
+++ server.js
@@ -132,12 +132,13 @@ process.on('SIGINT', function () {
 
 
 var userSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    password: String,
-    phone: String,
-    gender: String,
-    createdOn: { type: Date, 'default': Date.now }
+    "name": String,
+    "email": String,
+    "password": String,
+    "phone": String,
+    "gender": String,
+    "createdOn": { type: Date, 'default': Date.now }, 
+    "activeSince" : Date
 });
 var userModel = mongoose.model("users", userSchema);
 
@@ -153,11 +154,12 @@ app.use("/", express.static(path.resolve(path.join(__dirname, "public"))));
 
 app.post("/signup", (req, res, next) => {
 
-    if (!req.body.name
+    if (
+           !req.body.name
         || !req.body.email
         || !req.body.password
         || !req.body.number
-        || !req.body.gender) {
+        || !req.body.gender ) {
 
         res.status(403).send(`
             please send name, email, passwod, phone and gender in json body.
@@ -333,47 +335,4 @@ app.post("/login", (req, res, next) => {
     app.listen(PORT, () => {
         console.log("server is running on: ", PORT);
     })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//     var newUser = new userModel({
-//         "name": req.body.name,
-//         "email": req.body.email,
-//         "password": req.body.password,
-//         "phone": req.body.phone,
-//         "gender": req.body.gender,
-//     })
-
-//     newUser.save((err, data) => {
-//         if (!err) {
-//             res.send("user created")
-//         } else {
-//             console.log(err);
-//             res.status(500).send("user create error, " + err)
-//         }
-//     });
-// })
-
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-//     console.log("server is running on: ", PORT);
-// })
+})              
